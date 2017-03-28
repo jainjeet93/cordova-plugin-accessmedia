331a744bacda417c9b18cfca1efeb80c890e06ae
Mohit <mohitpatil546@gmail.com>
2017-03-29 04:14:10 +0530

    get media function added

--- src/android/AccessMedia.java
+++ src/android/AccessMedia.java
@@ -6,6 +6,9 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.apache.cordova.LOG;
+import android.provider.MediaStore;
+import android.database.Cursor;
 
 public class AccessMedia extends CordovaPlugin {
 
@@ -20,10 +23,24 @@ public class AccessMedia extends CordovaPlugin {
 	}
 
 	private void coolMethod(String message, CallbackContext callbackContext) {
-		if (message != null && message.length() > 0) {
-			callbackContext.success(message);
-		} else {
-			callbackContext.error("Expected one non-empty string argument.");
+		// if (message != null && message.length() > 0) {
+		// 	callbackContext.success(message);
+		// } else {
+		// 	callbackContext.error("Expected one non-empty string argument.");
+		// }
+		final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+		final String orderBy = MediaStore.Images.Media._ID;
+		Cursor cursor = this.cordova.getActivity().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, orderBy);
+		int count = cursor.getCount();
+
+		String[] arrPath = new String[count];
+
+		for (int i = 0; i < count; i++) {
+			cursor.moveToPosition(i);
+			int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+			arrPath[i]= cursor.getString(dataColumnIndex);
+			// Log.d("PATH", arrPath[i]);
 		}
+		callbackContext.success(arrPath[0]);
 	}
 }
--- www/AccessMedia.js
+++ www/AccessMedia.js
@@ -1,5 +1,20 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
+
+function accessMedia(){
+}
+
+accessMedia.prototype.coolMethod = function(arg0, success, error) {
 	exec(success, error, "AccessMedia", "coolMethod", [arg0]);
 };
+
+accessMedia.install = function () {
+	if (!window.plugins) {
+		window.plugins = {};
+	}
+
+	window.plugins.accessMedia = new accessMedia();
+	return window.plugins.accessMedia;
+};
+
+cordova.addConstructor(accessMedia.install);
